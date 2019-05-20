9939d13d69791b20dae045e6536ae77126dffa97
purisha <32126374+purisha@users.noreply.github.com>
2019-05-20 18:09:06 +0430

    Add files via upload

---
+++ my-work/src/app/helpers/auth.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { share, map } from 'rxjs/operators';
+import { JwtHelperService } from ' angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient, private decoder: JwtHelperService) {}
+
+  refreshToken(): Observable<string> {
+    const url = 'https:\/\/api.tsetab.com\/api\/book\/EmployeeList';
+    // append refresh token if you have one
+    const refreshToken = localStorage.getItem('refreshToken');
+    const expiredToken = localStorage.getItem('token');
+
+    return this.http
+      .get(url, {
+        headers: new HttpHeaders()
+          .set('refreshToken', refreshToken)
+          .set('token', expiredToken),
+        observe: 'response'
+      })
+      .pipe(
+        share(), // <========== YOU HAVE TO SHARE THIS OBSERVABLE TO AVOID MULTIPLE REQUEST BEING SENT SIMULTANEOUSLY
+        map(res => {
+          const token = res.headers.get('token');
+          const newRefreshToken = res.headers.get('refreshToken');
+
+          // store the new tokens
+          localStorage.setItem('refreshToken', newRefreshToken);
+          localStorage.setItem('token', token);
+
+          return token;
+        })
+      );
+  }
+
+  getToken(): Observable<string> {
+    const token = localStorage.getItem('token');
+    const isTokenExpired = this.decoder.isTokenExpired(token);
+
+    if (!isTokenExpired) {
+      return of(token);
+    }
+
+    return this.refreshToken();
+  }
+
+  // other auth methods
+}
